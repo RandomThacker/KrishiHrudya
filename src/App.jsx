b02e7860775ae63a5cb5058e407be43b59f353fc
RandomThacker <randomthacker@gmail.com>
2024-02-27 14:44:08 +0530

    data added

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         <Route path='about' element={<AboutPage />} />
         <Route path='cart' element={<CartPage />} />
         <Route path='products/:id' element={<SingleProductPage />} />
-        {/* <Route path='products' element={<ProductsPage />} /> */}
+        <Route path='products' element={<ProductsPage />} />
         <Route
           path='checkout'
           element={
---
+++ src/utils/data.js
@@ -0,0 +1,51 @@
+export const data = [
+    {
+        name : "SMART SINGLE PHASE MOTOR STARTER - 2 HP TO 5 HP",
+        price: "11,500",
+        id: "183001",
+        image: "https://cloudcardz.com/assets/images/cards/krishi_hrudya/product_1.jpg",
+        description: "Krishi Hrudya controllers offer connectivity over 4G wireless mobile technology. It requires a data plan to work and doesn't work on call or SMS"
+    },
+    {
+        name : "7.5HP SMART THREE PHASE MOTOR STARTER DOL (SINGLE CABLE MOTOR)",
+        price: "11,500",
+        id: "183002",
+        image: "https://cloudcardz.com/assets/images/cards/krishi_hrudya/product_2.jpg",
+        description: "Krishi Hrudya controllers offer connectivity over 4G wireless mobile technology. It requires a data plan to work and doesn't work on call or SMS"
+    },
+    {
+        name : "10HP SMART THREE PHASE MOTOR STARTER DOL (SINGLE CABLE MOTOR)",
+        price: "12,000",
+        id: "183003",
+        image: "https://cloudcardz.com/assets/images/cards/krishi_hrudya/product_3.jpg",
+        description: "Krishi Hrudya controllers offer connectivity over 4G wireless mobile technology. It requires a data plan to work and doesn't work on call or SMS"
+    },
+    {
+        name : "10HP SMART THREE PHASE MOTOR STARTER STAR DELTA (DOUBLE CABLE MOTOR)",
+        price: "15,000",
+        id: "183004",
+        image: "https://cloudcardz.com/assets/images/cards/krishi_hrudya/product_4.jpg",
+        description: "Krishi Hrudya controllers offer connectivity over 4G wireless mobile technology. It requires a data plan to work and doesn't work on call or SMS"
+    },
+    {
+        name : "15HP SMART THREE PHASE MOTOR STARTER STAR DELTA (DOUBLE CABLE MOTOR)",
+        price: "18,000",
+        id: "183005",
+        image: "https://cloudcardz.com/assets/images/cards/krishi_hrudya/product_4.jpg",
+        description: "Krishi Hrudya controllers offer connectivity over 4G wireless mobile technology. It requires a data plan to work and doesn't work on call or SMS"
+    },
+    {
+        name : "20HP SMART THREE PHASE MOTOR STARTER STAR DELTA (DOUBLE CABLE MOTOR)",
+        price: "20,000",
+        id: "183006",
+        image: "https://cloudcardz.com/assets/images/cards/krishi_hrudya/product_4.jpg",
+        description: "Krishi Hrudya controllers offer connectivity over 4G wireless mobile technology. It requires a data plan to work and doesn't work on call or SMS"
+    },
+    {
+        name : "3HP TO 25HP SMART THREE PHASE MOTOR CONTROLLER FOR 3RD PARTY INTERFACE",
+        price: "9,500",
+        id: "183007",
+        image: "https://cloudcardz.com/assets/images/cards/krishi_hrudya/product_7.JPG",
+        description: "Krishi Hrudya controllers offer connectivity over 4G wireless mobile technology. It requires a data plan to work and doesn't work on call or SMS"
+    },
+]
